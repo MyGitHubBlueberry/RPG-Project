a8255780be0911f4df5178d16644856fa7704d9a
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2023-07-01 13:08:04 +0300

    cross scene portal reference;

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -11,6 +11,7 @@ namespace RPG.SceneManagement
    {
       [SerializeField] private Scene targetScene;
       [SerializeField] private Transform spawnPoint;
+      [SerializeField] private DestionationIdentifier destination;
 
       private enum Scene
       {
@@ -19,39 +20,54 @@ namespace RPG.SceneManagement
          Sandbox2Scene,
       }
 
+      private enum DestionationIdentifier
+      {
+         None, A, B, C,
+      }
+
+
       private void OnTriggerEnter(Collider other)
       {
          if(other.gameObject.tag != Tag.Player.ToString()) return;
-
-         if(targetScene != Scene.None)
-         {
-            StartCoroutine(Transition());
-         }
+         
+         StartCoroutine(Transition());
       }
 
       private IEnumerator Transition()
       {
+         if(targetScene == Scene.None)
+         {
+            Debug.LogError("<color=orange>Scene to load</color> not set");
+            yield break;
+         }
+
          DontDestroyOnLoad(gameObject);
          yield return SceneManager.LoadSceneAsync(targetScene.ToString());
 
-         Portal linkedPortal = GetLinkedPortal();
-         UpdatePlayer(linkedPortal);
+         Portal destination = GetOtherPortal();
+         UpdatePlayer(destination);
 
          Destroy(gameObject);
       }
 
-      private void UpdatePlayer(Portal linkedPortal)
+      private void UpdatePlayer(Portal destination)
       {
          GameObject player = GameObject.FindGameObjectWithTag(Tag.Player.ToString());
-         player.GetComponent<NavMeshAgent>().Warp(linkedPortal.spawnPoint.position);
-         player.transform.rotation = linkedPortal.spawnPoint.rotation;
+         player.GetComponent<NavMeshAgent>().Warp(destination.spawnPoint.position);
+         player.transform.rotation = destination.spawnPoint.rotation;
       }
 
-      private Portal GetLinkedPortal()
+      private Portal GetOtherPortal()
       {
+         if(destination == DestionationIdentifier.None) 
+         {
+            Debug.LogError("<color=orange>Scene to load</color> not set");
+            return null;
+         }
+
          foreach(Portal portal in FindObjectsOfType<Portal>())
          {
-            if(portal == this) continue;
+            if(portal.destination != destination || portal == this) continue;
 
             return portal;
          }
