a3becb4111d3584f5afef68aa5060d65b300b395
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2023-07-05 12:23:04 +0300

    Improve Projectile Behaviour

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,22 +6,30 @@ namespace RPG.Combat
    public class Projectile : MonoBehaviour
    {
       [SerializeField] private float speed;
+      [SerializeField] private bool isHoming;
 
       private Health target;
       private float damage = 0f;
 
+      private void Start()
+      {
+         transform.LookAt(GetAimLocation());
+      }
 
       private void Update()
       {
          if(target == null) return;
 
-         transform.LookAt(GetAimLocation());
+         if(isHoming && !target.GetIsDead()) transform.LookAt(GetAimLocation());
+
          transform.Translate(Vector3.forward * speed * Time.deltaTime);
       } 
 
       private void OnTriggerEnter(Collider other)
       {
          if(other.GetComponent<Health>() != target) return;
+         if(target.GetIsDead()) return;
+
          target.TakeDamage(damage);
          Destroy(gameObject);
       }
