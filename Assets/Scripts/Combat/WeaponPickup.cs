9650106628c57dd46abf3a1d2a72f067f890c0e2
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2023-07-06 20:15:29 +0300

    pickup respawning; terrain fixes

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using RPG.Tags;
 using UnityEngine;
 
@@ -6,13 +8,30 @@ namespace RPG.Combat
    public class WeaponPickup : MonoBehaviour
    {
       [SerializeField] private Weapon pickupWeapon;
+      [SerializeField] private float respawnTime = 3f;
 
       private void OnTriggerEnter(Collider other)
       {
          if(other.gameObject.tag == Tag.Player.ToString())
          {
             other.GetComponent<Fighter>().EquipWeapon(pickupWeapon);
-            Destroy(gameObject);
+            StartCoroutine(HideForSeconds(respawnTime));
+         }
+      }
+
+      private IEnumerator HideForSeconds(float seconds)
+      {
+         ShowPickup(false);
+         yield return new WaitForSeconds(seconds);
+         ShowPickup(true);
+      }
+
+      private void ShowPickup(bool show)
+      {
+         GetComponent<CapsuleCollider>().enabled = show;
+         foreach(Transform child in transform)
+         {
+            child.gameObject.SetActive(show);
          }
       }
    }
