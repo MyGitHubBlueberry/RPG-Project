0329be4bedfae9e4bd9aa268158b1a06935134c6
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2023-07-08 15:01:30 +0300

    Saving Experience Points

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,14 +1,26 @@
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Attributes
 {   
-   public class Experience : MonoBehaviour
+   public class Experience : MonoBehaviour, ISaveable
    {
       [SerializeField] private float experience = 0f;
+
+      
 
       public void GainExperience(float experience)
       {
          this.experience += Mathf.Max(experience, 0f);
+      }
+      public JToken CaptureAsJToken()
+      {
+         return JToken.FromObject(experience);
+      }
+      public void RestoreFromJToken(JToken state)
+      {
+         experience = state.ToObject<float>();
       }
    }
 }
