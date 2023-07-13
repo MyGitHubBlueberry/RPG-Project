88be70b97859c56bd0fd57a6f267f7ebd3b6df8a
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2023-07-13 16:30:42 +0300

    better progression in inspector

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using MyBox;
 
 namespace RPG.Stats
 {
@@ -12,14 +12,23 @@ namespace RPG.Stats
       private const int PENULTIMATE_LEVEL = 99;
 
       [SerializeField] private CharacterClass characterClass;
+      [SerializeField] private bool isDependsOnProgression;
+
+      [ConditionalField(nameof(isDependsOnProgression))]
+      [SerializeField] private Progression baseProgression;
       [SerializeField] private ProgressionStat[] stats;
 
-      [ExecuteAlways]
       [Serializable]
       private class ProgressionStat
       {
          public Stat stat;
+         public bool isDependsOnProgression;
+
+         [ConditionalField(nameof(isDependsOnProgression), true)]
          public StatFormula formula;
+
+         [ConditionalField(nameof(isDependsOnProgression))]
+         public float statMultiplier = 1;
       }
 
       [Serializable]
@@ -29,7 +38,6 @@ namespace RPG.Stats
          [SerializeField] float level100 = 500;
 
          float deltaPerLevel;
-         bool isDeltaCalculated;
 
          private void CalculateDelta()
          {
@@ -44,16 +52,55 @@ namespace RPG.Stats
             if(level<0) level = 0;
             return level1 + deltaPerLevel * level;
          }
+
          public int EvaluateAsInt(int level) => Mathf.RoundToInt(Evaluate(level));
+         public void GetLevels(out float level1, out float level100)
+         {
+            level1 = this.level1;
+            level100 = this.level100;
+         }
+
+         public void SetLevels(float level1, float level100, float multiplier)
+         {
+            this.level1 = level1 * multiplier; 
+            this.level100 = level100 * multiplier;
+         }
       }
 
       public float GetStatEvaluation(Stat stat, int level)
       {
-         var statEvaluation = from progressionStat in stats 
-                              where progressionStat.stat == stat
-                              select progressionStat.formula.EvaluateAsInt(level);
+         if(isDependsOnProgression)
+         {
+            SetLevels(stat);
+         }
 
-         return statEvaluation.First();
+         var statEvaluation = GetProgressionStat(this, stat).formula.EvaluateAsInt(level);
+
+         return statEvaluation;
+      }
+
+      private void SetLevels(Stat stat)
+      {
+         if (baseProgression == null)
+         {
+            Debug.LogError("baseProgression can not be null");
+         }
+         var currentProgressionStat = GetProgressionStat(this, stat);
+
+         if(!currentProgressionStat.isDependsOnProgression) return;
+
+         var baseStatFormula = GetProgressionStat(baseProgression, stat).formula;
+         baseStatFormula.GetLevels(out float level1, out float level100);
+
+         currentProgressionStat.formula.SetLevels(level1, level100, currentProgressionStat.statMultiplier);
+      }
+
+      private ProgressionStat GetProgressionStat(Progression progression, Stat stat)
+      {
+         var output = from progressionStat in progression.stats 
+                      where progressionStat.stat == stat
+                      select progressionStat;
+         return output.First();
       }
 
       public int GetPenultimateLevel()
